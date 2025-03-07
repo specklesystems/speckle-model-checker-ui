8612ca3385622238bc293cc4ab2e785f6a13f11c
Jonathon Broughton <760691+jsdbroughton@users.noreply.github.com>
2025-03-07 14:21:59 +0000

    Update main.py

--- functions/main.py
+++ functions/main.py
@@ -1,4 +1,4 @@
-# Welcome to Cloud Functions for Firebase for Python
+# Welcome to Cloud Functions for Firebase for Python!
 
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
